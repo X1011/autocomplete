14a5e340176e4be49ac8b9714c8fedea45adcaf7
X1011 <xx1011@gmail.com>
2015-01-09 07:42:03 +0000

    Mocha & Chai with jspm

---
+++ app/runner.js
@@ -0,0 +1,4 @@
+import mocha from 'mocha';
+mocha.setup('bdd');
+import '~/test';
+mocha.run();
---
+++ app/test.js
@@ -0,0 +1,8 @@
+import chai from 'chai';
+var should = chai.should();
+
+describe('test', function() {
+	it("uses Chai's 'should' interface", function() {
+		true.should.be.true;
+	});
+});
