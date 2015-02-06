cfc76e548484c7d0e38875576177b6f2e62601bc
X1011 <xx1011@gmail.com>
2015-02-06 04:37:19 +0000

    filter function

--- app/autocomplete.js
+++ app/autocomplete.js
@@ -1,40 +1,47 @@
 (function() {
 
-window.addEventListener('load', function() {
-	
-	var inputs = document.getElementsByClassName('autocomplete');
+	window.addEventListener('load', function() {
 
-	for (var i = 0, input = inputs[0];
-		     i < inputs.length;
-		     i++,   input = inputs[i]) {
+		var inputs = document.getElementsByClassName('autocomplete');
 
-		input.setAttribute('autocomplete', 'off'); //prevent browser's native autocomplete from interfering
-		
-		input.addEventListener('focus', createDropdown);
-		input.addEventListener('blur', removeDropdown);
-	}
-});
-	
-var candidates = ['apple', 'orange', 'banana'];
-var dropdowns = [];
-	
-function createDropdown(e) {
-	var input = e.target;
-	var dropdown = document.createElement('div');
-	dropdowns[input] = dropdown;
-	
-	candidates.forEach(function(candidate) {
-		var element = document.createElement('div');
-		element.textContent = candidate;
-		dropdown.appendChild(element);
+		for (var i = 0, input = inputs[0];
+				 i < inputs.length;
+				 i++,   input = inputs[i]) {
+
+			input.setAttribute('autocomplete', 'off'); //prevent browser's native autocomplete from interfering
+
+			input.addEventListener('focus', createDropdown);
+			input.addEventListener('blur', removeDropdown);
+		}
 	});
-	
-	input.parentNode.insertBefore(dropdown, input.nextSibling);
-}
-	
-function removeDropdown(e) {
-	var input = e.target;
-	input.parentNode.removeChild(dropdowns[input]);
-}
+
+	var candidates = ['apple', 'orange', 'banana'];
+	var dropdowns = [];
+
+	function createDropdown(e) {
+		var input = e.target;
+		var dropdown = document.createElement('div');
+		dropdowns[input] = dropdown;
+
+		filter(candidates, input.value).forEach(function(candidate) {
+			var element = document.createElement('div');
+			element.textContent = candidate;
+			dropdown.appendChild(element);
+		});
+
+		input.parentNode.insertBefore(dropdown, input.nextSibling);
+	}
+
+	function removeDropdown(e) {
+		var input = e.target;
+		input.parentNode.removeChild(dropdowns[input]);
+	}
 
 })();
+
+//exposing this so the test can access it
+function filter(candidates, input) {
+	return candidates.filter(function(candidate) {
+		return candidate.indexOf(input) != -1;
+	});
+}
--- app/test.js
+++ app/test.js
@@ -1,7 +1,23 @@
 var should = chai.should();
 
-describe('test', function() {
-	it("uses Chai's 'should' interface", function() {
-		true.should.be.true;
+describe('filter', function() {
+	it('does nothing when the input is empty', function() {
+		filter(['b'], '').should.deep.equal(['b']);
+	});
+
+	it('matches a candidate that is the same as the input', function() {
+		filter(['b'], 'b').should.deep.equal(['b']);
+	});
+
+	it('does not match a candidate that does not contain the input', function() {
+		filter(['b'], 'a').should.deep.equal([]);
+	});
+	
+	it('matches a prefix of a candidate', function() {
+		filter(['apple', 'orange', 'banana'], 'ap').should.deep.equal(['apple']);
+	});
+	
+	it('matches a string in the middle of a candidate', function() {
+		filter(['apple', 'orange', 'banana'], 'an').should.deep.equal(['orange', 'banana']);
 	});
 });
