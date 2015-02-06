1190d752a9e7369c1840e3520d72a8c608de7b13
X1011 <xx1011@gmail.com>
2015-02-06 04:03:44 +0000

    process each autocomplete element for reals

--- app/autocomplete.js
+++ app/autocomplete.js
@@ -1,11 +1,19 @@
 (function() {
 
-var inputs = document.getElementsByClassName('autocomplete');
+window.addEventListener('load', function() {
+	
+	var inputs = document.getElementsByClassName('autocomplete');
 
-for (var i = 0, input = inputs[0]; i < inputs.length; i++) {
-	
-	input.setAttribute('autocomplete', 'off'); //prevent browser's native autocomplete from interfering
-	
-}
-	
+	for (var i = 0, input = inputs[0];
+		     i < inputs.length;
+		     i++,   input = inputs[i]) {
+
+		input.setAttribute('autocomplete', 'off'); //prevent browser's native autocomplete from interfering
+		
+		input.addEventListener('focus', function(e) {
+			console.log(e);
+		});
+	}
+});
+
 })();
