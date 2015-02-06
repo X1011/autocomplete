c468bea08a6d3d96be03760febce729941dd876c
X1011 <xx1011@gmail.com>
2015-02-06 04:18:07 +0000

    insert dropdown

--- app/autocomplete.js
+++ app/autocomplete.js
@@ -10,10 +10,23 @@ window.addEventListener('load', function() {
 
 		input.setAttribute('autocomplete', 'off'); //prevent browser's native autocomplete from interfering
 		
-		input.addEventListener('focus', function(e) {
-			console.log(e);
-		});
+		input.addEventListener('focus', createDropdown);
 	}
 });
+	
+var candidates = ['apple', 'orange', 'banana'];
+	
+function createDropdown(e) {
+	var input = e.target;
+	var dropdown = document.createElement('div');
+	
+	candidates.forEach(function(candidate) {
+		var element = document.createElement('div');
+		element.textContent = candidate;
+		dropdown.appendChild(element);
+	});
+	
+	input.parentNode.insertBefore(dropdown, input.nextSibling);
+}
 
 })();
